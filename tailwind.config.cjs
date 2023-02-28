e85256f3a41b0cbd992be62f099ccbc213efc755
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2023-02-28 15:16:00 +0100

    Tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeInTop: {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInBottom: {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        scaleX: {
+          '0%': { transform: 'scaleX(0)' },
+          '100%': { transform: 'scaleX(1)' },
+        },
+        hide: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+        reveal: {
+          '0%': { transform: 'scale(0.6)', opacity: 0 },
+          '100%': { transform: 'scale(0.75)', opacity: 1 },
+        },
+      },
+      animation: {
+        fadeInTop: 'fadeInTop 0.6s 0.3s forwards',
+        fadeInBottom: 'fadeInBottom 0.6s 0.3s forwards',
+        scaleX: 'scaleX 0.6s 0.2s forwards',
+        hide: 'hide 0.5s 0.1s forwards',
+        reveal: 'reveal 0.3s 0.5s forwards',
+      },
+    },
+  },
+  plugins: [],
+};
